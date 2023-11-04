5a4732ffb4b6e1afd73c9818b2373b762a7161e4
Kim, Paul 01 <pmk349@nyu.edu>
2023-11-04 14:45:10 -0400

    update emails

--- src/pages/Visit.js
+++ src/pages/Visit.js
@@ -12,9 +12,9 @@ const Visit = () => {
         <p>Sat 8AM - 4PM</p>
         <p>Sun CLOSED</p>
         <h3>Phone</h3>
-        <p><a href="tel:5555555555">(212) 335-0333</a></p>
+        <p><a href="tel:2123350333">(212) 335-0333</a></p>
         <h3>Email</h3>
-        <p><a href="mailto:coffee.grounds@realemail.com">coffee.grounds@realemail.com</a></p>
+        <p><a href="mailto:kingsstreetcoffee@gmail.com">kingsstreetcoffee@gmail.com</a></p>
         <h3>Location</h3>
         <p>121 W 30th ST, New York, NY 10001</p>
       </section>
